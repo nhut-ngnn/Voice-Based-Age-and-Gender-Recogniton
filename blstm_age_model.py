eb3cc8ff625f14afd26002adc80676e93f9c44fc
Nguyễn Minh Nhựt <138198155+nhut-ngnn@users.noreply.github.com>
2024-05-24 00:45:52 +0700

    Add files via upload

--- blstm_age_model.py
+++ blstm_age_model.py
@@ -2,7 +2,7 @@
 import keras_metrics as km
 from keras import metrics
 from keras import Sequential
-from keras.layers import LSTM, Dense, Dropout, BatchNormalization
+from keras.layers import LSTM, Dense, Dropout, BatchNormalization, Bidirectional
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 
@@ -12,13 +12,10 @@ NUM_FEATURES = 145
 
 
 def lstm_age_model(num_labels):
-    model = Sequential()
-    model.add(LSTM(256, input_shape=(35, NUM_FEATURES), return_sequences=True, dropout=0.3))
-    model.add(LSTM(256, dropout=0.3))
+    model = Sequential() 
+    model.add(Bidirectional(LSTM(256, dropout=0.3, return_sequences=True), input_shape=(35, NUM_FEATURES)))
+    model.add(Bidirectional(LSTM(256, dropout=0.3)))
     model.add(Dense(128 * 2, activation='relu'))
-    model.add(Dropout(0.3))
-    model.add(BatchNormalization())
-    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.3))
     model.add(BatchNormalization())
     model.add(Dense(128, activation='relu'))
@@ -28,15 +25,14 @@ def lstm_age_model(num_labels):
     model.add(Dropout(0.3))
     model.add(BatchNormalization())
     model.add(Dense(num_labels, activation='softmax'))
-    optimizer=Adam()
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer,
-                   metrics=['accuracy'])#, metrics.F1Score(threshold=0.5)])
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(),
+                  metrics=['accuracy'])
     return model
 
 
 def main_class_age_train():
     dataset = "C:/Users/admin/Documents/Voice_Based_Age_Gender_and_Emotion/New_Project/age_data_clean_small"  
-    model = "C:/Users/admin/Documents/Voice_Based_Age_Gender_and_Emotion/New_Project/model/lstm_age_"
+    model = "C:/Users/admin/Documents/Voice_Based_Age_Gender_and_Emotion/New_Project/model/blstm_age_"
     early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 
     train_multi_epoch(dataset, model + str(NUM_FEATURES),
