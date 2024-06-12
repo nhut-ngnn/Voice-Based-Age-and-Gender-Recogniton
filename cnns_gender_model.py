1796fb1ed466ecb63bb647bb86c31c4703afd0bd
Nguyễn Minh Nhựt <138198155+nhut-ngnn@users.noreply.github.com>
2024-06-12 22:44:10 +0700

    Add files via upload

--- cnns_gender_model.py
+++ cnns_gender_model.py
@@ -1,31 +1,24 @@
 import tensorflow as tf
 
 from keras import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, BatchNormalization
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
 
 from models import train_multi_epoch, train_deepnn
 
-NUM_FEATURES = 145 
+NUM_FEATURES = 54 
 
 
-def cnns_gender_model(num_labels, learning_rate =0.00001):
+def cnns_gender_model(num_labels):
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(35, NUM_FEATURES, 1)))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(35, NUM_FEATURES)))
+    model.add(MaxPooling1D(pool_size=2))
     model.add(Dropout(0.3))
-
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(0.3))
-
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D((2, 2)))
+    
+    model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
     model.add(Dropout(0.3))
 
     model.add(Flatten())
@@ -38,9 +31,8 @@ def cnns_gender_model(num_labels, learning_rate =0.00001):
     model.add(BatchNormalization())
 
     model.add(Dense(num_labels, activation='softmax'))
-    # optimizer = Adam(learning_rate=learning_rate)
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(),
-                  metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
     return model
 
 
@@ -51,7 +43,7 @@ def main_class_gender_train():
 
     train_multi_epoch(dataset, model + str(NUM_FEATURES),
                       cnns_gender_model, train_deepnn,
-                      num_epoch_start=50,
+                      num_epoch_start=15,
                       num_features=NUM_FEATURES,
                       file_prefix="gender",
                       callbacks=[early_stopping])
