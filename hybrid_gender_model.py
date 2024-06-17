38b84721c4f3d74683af848720941aa843a8bdf5
Nhut Minh Nguyen <138198155+nhut-ngnn@users.noreply.github.com>
2024-06-17 11:10:17 +0700

    Add files via upload

--- hybrid_gender_model.py
+++ hybrid_gender_model.py
@@ -13,11 +13,13 @@ def cnn_bilstm_gender_model(num_labels):
     model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(35, NUM_FEATURES)))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Dropout(0.3))
-    model.add(Bidirectional(LSTM(256, dropout=0.3, return_sequences=True)))
-    model.add(Bidirectional(LSTM(256, dropout=0.3)))
-    model.add(Dense(128 * 2, activation='relu'))
+    model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
     model.add(Dropout(0.3))
-    model.add(BatchNormalization())
+    
+    model.add(Bidirectional(LSTM(128, dropout=0.3, return_sequences=True)))
+    model.add(Bidirectional(LSTM(128, dropout=0.3)))
+  
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.3))
     model.add(BatchNormalization())
@@ -25,7 +27,7 @@ def cnn_bilstm_gender_model(num_labels):
     model.add(Dropout(0.3))
     model.add(BatchNormalization())
     model.add(Dense(num_labels, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(0.0001), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
     
     return model
@@ -33,11 +35,11 @@ def cnn_bilstm_gender_model(num_labels):
 def main_class_gender_train():
     dataset = "C:/Users/admin/Documents/Voice_Based_Age_Gender_and_Emotion/New_Project/gender_data_clean"
     model = "C:/Users/admin/Documents/Voice_Based_Age_Gender_and_Emotion/New_Project/model/hybrid_gender_"
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
     train_multi_epoch(dataset, model + str(NUM_FEATURES),
                       cnn_bilstm_gender_model, train_deepnn,
-                      num_epoch_start=15,
+                      num_epoch_start=18,
                       num_features=NUM_FEATURES,
                       file_prefix="gender",
                       callbacks=[early_stopping])
